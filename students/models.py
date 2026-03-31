65c0a7cc3f9795ca0510be1349a37feb12523ebe
davinoroes <davinoroes1918@gmail.com>
2026-03-31 13:17:07 -0300

    Criação inicial do model Student

--- students/models.py
+++ students/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from cpf_field.models import CPFField
 
-# Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    cpf = CPFField('cpf') #com isso já valido o cpf
+    curse = models.ForeignKey(...) #vou colocar uma foreign key com o model do curso (vou fazer uma app para curso)
+    
+    grade = models.ManyToManyField(...) #manytomany com o model Materias, e vou usar o throug com o model Matrícula
+
+    def __str__(self):
+        return self.name
+
