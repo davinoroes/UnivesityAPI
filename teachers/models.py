9a17320cf179a5c8fd7ced2ba7cd1cb882b19b97
davinoroes <davinoroes1918@gmail.com>
2026-04-01 11:06:14 -0300

    Adição foreign key professor-curso

--- teachers/models.py
+++ teachers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from cpf_field.models import CPFField
+from course.models import Curse
 
 TITULACOES = [ ("GR", "Graduado"),
     ("ME", "Mestre"),
@@ -11,9 +12,10 @@ class Teacher(models.Model):
     cpf = CPFField('cpf')
     email = models.EmailField(unique=True)
     academic_degree = models.CharField(max_length=2, choices=TITULACOES)
+    curse = models.ForeignKey(Curse, on_delete=models.PROTECT, related_name='teachercurse')
     
 
     def __str__(self):
-        self.name
+        return self.name
 
     
