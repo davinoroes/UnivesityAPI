722b6bc30a3aa94346f2467f923f3a8c905047c2
davinoroes <davinoroes1918@gmail.com>
2026-04-01 09:19:02 -0300

    Att app course

---
+++ course/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+
+COURSE_CHOICES = [
+    ('CS', 'Ciência da Computação'),
+    ('ENGC', 'Engenharia Civil'),
+    ('MED', 'Medicina'),
+    ('DIR', 'Direito'),
+    ('ADM', 'Administração'),
+    ('ECO', 'Economia'),
+    ('PSQ', 'Psicologia'),
+    ('ENF', 'Enfermagem'),
+    ('ARQ', 'Arquitetura'),
+    ('PED', 'Pedagogia'),
+]
+
+
+class Curse(models.Model):
+    name = models.CharField(choices=COURSES_CHOICES)
+    curse_code = models.CharField(max_length=6) 
+    semesters = models.IntegerField(validators=[
+        MinValueValidator(8, "A duração mínima dos cursos são 8 semestres "),
+        MaxValueValidator(12, "A duração máxima dos cursos são 12 semestres"),
+    ])
+
+
+    def __str__(self):
+        self.name
