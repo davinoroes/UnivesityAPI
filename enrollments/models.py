51ee921e7aa4feb1a59254ade56849c2645dc5da
davinoroes <davinoroes1918@gmail.com>
2026-04-14 17:32:40 -0300

    Criação model Enrollment

--- enrollments/models.py
+++ enrollments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from students.models import Student
+from discipline.models import Discipline
 
-# Create your models here.
+class Enrollment(models.Model):
+    students = models.ForeignKey(Student,on_delete=models.CASCADE)
+    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE)
+
+    semester = models.CharField(max_length=6) #tipo 2026.1
+    final_note = models.IntegerField(null=True,blank=True)
+    approved = models.BooleanField(default=False)
+
