b86731e173f44dd29ca0219e228992823fca511e
davinoroes <davinoroes1918@gmail.com>
2026-04-14 16:36:13 -0300

    Criação model discipline

--- discipline/models.py
+++ discipline/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
+from teachers.models import Teacher
 
-# Create your models here.
+class Discipline(models.Model):
+    name = models.CharField(max_length=200)
+    discipline_code = models.CharField(max_length=9)
+    credit_hours = models.IntegerField(validators=[
+        MinValueValidator(32, "A duração mínima de uma disciplina é 32 horas"),
+        MaxValueValidator(96, "A duração máxima de uma disciplina é 96 horas"),
+    ])
+    teacher = models.ForeignKey(Teacher,on_delete=models.SET_NULL,null=True,related_name='disciplineteacher')
+
+    def __str__(self):
+        return self.name
